4376e33c2c30730445e81660171b85d5251d35e2
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-11-17 16:19:25 +0300

    Slicing the UI in posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Addfriends from './Addfriends';
 
 import Post from './Post';
 
@@ -33,13 +34,35 @@ function Posts() {
 				postImage:
 					"https://res.cloudinary.com/itgenius/image/upload/v1668007538/pexels-martin-boh%C3%A1%C4%8D-10288457_uwpcbd.jpg",
 			},
+			{
+				id: 4,
+				name: "muwonge lawrence",
+				message: "how are you test one two",
+				email: "muwongelawrence44@gmail.com",
+				timestamp: "8:00 am",
+				postImage:
+					"https://res.cloudinary.com/itgenius/image/upload/v1668007538/pexels-martin-boh%C3%A1%C4%8D-10288457_uwpcbd.jpg",
+			},
 		]);
 
 
     return (
 
-        <div className = "space-y-7">
-            { newPosts.map(({id , name ,message, email ,timestamp ,postImage })=> (
+        <div className = "space-y-4">
+            { newPosts.slice(0,2).map(({id , name ,message, email ,timestamp ,postImage })=> (
+                <Post 
+                 key = { id }
+                 name = { name }
+                 message = {message}
+                 email = {email}
+                 timestamp = {timestamp}
+                 postImage = {postImage}
+                 />
+            ))}
+
+              <Addfriends />
+
+			  { newPosts.slice(2,newPosts.length).map(({id , name ,message, email ,timestamp ,postImage })=> (
                 <Post 
                  key = { id }
                  name = { name }
